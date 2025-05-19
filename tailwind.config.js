d48682b552ca10d7f69da0c6c55d4731f3294bd8
youssef123877 <ymachhouri1@gmail.com>
2025-05-19 18:09:32 +0100

    Add files via upload
    1root

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+const {} = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  mode: "jit",
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  variants: {
+    extend: {},
+  },
+};
